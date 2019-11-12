7672b059ae1425f75be809dc010aa9433f7278c5
Sean Good <tree662@protonmail.com>
2019-11-12 12:51:29 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,11 @@
 require 'pry'
 
 def my_find(collection)
-
+  i = 0 
+  while i < collection.length 
+    if yield(collection[i]) #line6 can go behind return statement to be one line
+      return collection[i]  #its just not working right now 
+    end
+    i = i + 1 
+  end
 end
