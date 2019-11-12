adc083ad7ee131cbc93a97ec30d691c4b04148b0
Sean Good <tree662@protonmail.com>
2019-11-12 12:51:15 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,8 +3,8 @@ require 'pry'
 def my_find(collection)
   i = 0 
   while i < collection.length 
-    if yield(collection[i])
-      return collection[i]
+    if yield(collection[i]) #line6 can go behind return statement to be one line
+      return collection[i]  #its just not working right now 
     end
     i = i + 1 
   end
